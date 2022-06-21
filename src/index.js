ef59517a391d4470d95de29dc7b47ffd739d5fe1
zbl14 <ifthereisoneday@gmail.com>
2022-06-20 22:51:12 -0700

    add flipCard function hidden back-face

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import 'bootstrap';
-import './css/styles.css';
-import $ from 'jquery';
+// import 'bootstrap';
+// import './css/styles.css';
+// import $ from 'jquery';
+
+
+
+
+
 
 
 $(document).ready(function() {
-  
+  function flipCard(){
+    $(".memory-card").on("click", function(){
+        $(this).toggleClass("flip");
+    })
+  }
+  flipCard();
 });
