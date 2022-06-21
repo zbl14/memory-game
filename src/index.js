af4f00acbe6c43bef6f29d97dc33e3931cca443a
zbl14 <ifthereisoneday@gmail.com>
2022-06-20 23:48:15 -0700

    creat unflipCard function to removeClass flip

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@
 // import $ from 'jquery';
 
 
+function flipCard(){
+  $(".memory-card").on("click", function(){
+      $(this).addClass("flip");
+  })
+}
 
+function unflipCard(){
+  $(".memory-card").on("click", function(){
+    $(this).removeClass("flip");
+})
+}
 
 
 
 
 $(document).ready(function() {
-  function flipCard(){
-    $(".memory-card").on("click", function(){
-        $(this).toggleClass("flip");
-    })
-  }
+
   flipCard();
+
+
 });
