f74985bff65c2af7cf4677491a4bddc0c984f858
zbl14 <ifthereisoneday@gmail.com>
2022-07-07 07:42:11 -0700

    add checkMatch

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
-// import 'bootstrap';
-// import './css/styles.css';
-// import $ from 'jquery';
+import 'bootstrap';
+import './css/styles.css';
+import $ from 'jquery';
+// import { Card } from '..src/js/game.js'
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
-function flipCard(){
+let flipCard = () => {
   $(".memory-card").on("click", function(){
-      $(this).addClass("flip");
-  })
-}
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+      console.log(hasFlippedCard);
+      hasFlippedCard = true;
+      console.log(hasFlippedCard);
+      firstCard = this;
+      console.log(firstCard);
+      return;
+    }
+    console.log(hasFlippedCard);
+    secondCard = this;
+    console.log(secondCard);
+    hasFlippedCard = false;
+    console.log(hasFlippedCard);
+    checkMatch();
+  });
+};
 
-function unflipCard(){
-  $(".memory-card").on("click", function(){
-    $(this).removeClass("flip");
-})
-}
+let checkMatch = () => {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCard();
+};
+
+let disableCards = () => {
+  $(".memory-card").off("click", flipCard);
+};
+
+let unflipCard = () => {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+  }, 1500);
+};
+
+
+
+
+// function unflipCard(){
+//   $(".memory-card").on("click", function(){
+//     $(this).removeClass("flip");
+//   });
+// }
 
 
 
